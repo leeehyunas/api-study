7c816d17ba57e1715768f44b61b8bd6d318594fe
leeehyunas <nzzzzz0628@gmail.com>
2022-09-07 17:26:12 +0900

    title change

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App" />
-  <div>hello zzzzz</div>
+  <div>sweet tracker</div>
   <router-link>sweet-tracker</router-link>
   <router-view :key="$route.path" />
 </template>
